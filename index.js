3d6a433f8b1947c5b3a4e891725bc087a17306af
Scott Phillips <scottphillips1451@sbcglobal.net>
2018-06-21 20:50:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten (name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.shift(name);
+  return kittens;
+}
+
